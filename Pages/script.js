fd6832c4ca3339855de7efbee972156b010c593a
Luthfi Alwan Azhari <luthfialwan2@gmail.com>
2023-09-21 13:03:52 +0700

    fix banner-teks

--- Pages/script.js
+++ Pages/script.js
@@ -1,27 +1,26 @@
-const accordions = document.querySelectorAll('.accordion');
-      
-accordions.forEach(accordion => {
-  const header = accordion.querySelector('.accordion-header');
-  const content = accordion.querySelector('.accordion-content');
+const accordions = document.querySelectorAll(".accordion");
 
-  header.addEventListener('click', () => {
-    if (content.style.display === 'block') {
-      content.style.display = 'none';
+accordions.forEach((accordion) => {
+  const header = accordion.querySelector(".accordion-header");
+  const content = accordion.querySelector(".accordion-content");
+
+  header.addEventListener("click", () => {
+    if (content.style.display === "block") {
+      content.style.display = "none";
     } else {
-      accordions.forEach(item => {
-        item.querySelector('.accordion-content').style.display = 'none';
+      accordions.forEach((item) => {
+        item.querySelector(".accordion-content").style.display = "none";
       });
-      content.style.display = 'block';
+      content.style.display = "block";
     }
   });
 });
 
-
 function myFunction() {
-    var x = document.querySelector(".nav-links");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+  var navBar = document.querySelector(".nav-links");
+  if (navBar.style.display === "block") {
+    navBar.style.display = "none";
+  } else {
+    navBar.style.display = "block";
   }
+}
